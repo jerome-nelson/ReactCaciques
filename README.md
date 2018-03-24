c1d72465d5e63eb15fb883b0453c825fbbc04ab7
Jerome Nelson <jerome.nelson@netplaytv.com>
2018-03-24 17:55:15 +0000

    feat(project): Addition of prettier.

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a project using React.
+
+## Contains
+* Webpack Dev Server running Development Build of React
+* Injection of critical styles in Critical.scss
--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import AppContainer from './containers/app.container';
-import './styles/critical.scss';
 
 ReactDOM.render(
     <AppContainer />,
