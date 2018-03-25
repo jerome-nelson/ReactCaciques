723f526ad03a8cd6fc502feee202838638d2e16b
Jerome Nelson <jerome.nelson@netplaytv.com>
2018-03-25 22:05:19 +0100

    fix(react-style): Reversion of style injection to allow push.

--- src/app/containers/app.container.js
+++ src/app/containers/app.container.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import "../styles/critical.scss";
+
 class AppContainer extends Component {
   constructor() {
     super();
--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { hydrate } from "react-dom";
 import AppContainer from "./containers/app.container";
+import "../styles/critical.scss";
 
 hydrate(<AppContainer />, document.getElementById("root"));
