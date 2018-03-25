7c0b700c266b2ee653bf6e1f2ceddcb33ea256f7
Jerome Nelson <jerome.nelson@netplaytv.com>
2018-03-25 18:36:19 +0100

    refactor(entry): implicit import of render from react-dom.

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import AppContainer from "./containers/app.container";
 import "./styles/critical.scss";
 
-ReactDOM.render(<AppContainer />, document.getElementById("root"));
+render(<AppContainer />, document.getElementById("root"));
