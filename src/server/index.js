2a87bd195df0051ea42eb98cc8465de4170ead31
Jerome Nelson <jerome.nelson@netplaytv.com>
2018-04-02 20:05:12 +0100

    feat(ssr): Addition of Express Server.

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,18 @@
+import express from "express";
 import React from "react";
-import ReactDOMServer from "react-dom/server";
-import templateFn from "../templates";
+import { renderToString } from "react-dom/server";
 import AppContainer from "../app/containers/app.container";
-export default (req, res) => {
-  const html = ReactDOMServer.renderToString(<AppContainer />);
-  const template = templateFn(html);
-  res.send(template);
-};
+import templateFn from "../templates";
+
+const port = 3000;
+const server = express();
+
+server.get("/", (req, res) => {
+  const html = renderToString(<AppContainer />);
+  res.send(templateFn(html));
+});
+server.use("/assets", express.static("assets"));
+server.listen(port);
+
+// eslint-disable-next-line
+console.log(`Serving at http://localhost:${port}`);
