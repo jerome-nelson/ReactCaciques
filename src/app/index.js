4b5c37968347c9fd6c6ff28f492c30d263599ff9
Jerome Nelson <jerome.nelson@netplaytv.com>
2018-06-05 15:14:25 +0100

    feat(react-router-dom): Addition of routing sample for develop

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import { hydrate } from "react-dom";
-import AppContainer from "./containers/app.container";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import routes from "./routes";
 import "./styles/critical.scss";
 
-hydrate(<AppContainer />, document.getElementById("root"));
+hydrate(
+  <Router>
+    <Switch>
+      {routes.map((route, iteration) => (
+        <Route key={`route-${iteration}`} {...route} />
+      ))}
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
---
+++ src/app/routes.js
@@ -0,0 +1,10 @@
+import AppContainer from "./containers/app.container";
+
+const routes = [
+  {
+    path: "/",
+    component: AppContainer
+  }
+];
+
+export default routes;
